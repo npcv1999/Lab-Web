33cae22df2a6230bf3a83cfe9974fedf8bcada08
Administrator <Administrator@PC236>
2020-06-04 15:51:05 +0700

    thuc hanh 4-6-2020

---
+++ lab456/ViewModels/FutureDate.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace lab456.ViewModels
+{
+    public class FutureDate : ValidationAttribute
+    {
+        public override bool IsValid(object value)
+        {
+            DateTime dateTime
+            return base.IsValid(value);
+        }
+    }
+}
